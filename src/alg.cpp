daaa3210933566c02fb455145f65d3dfe0a64038
ErrorDa <99288583+ErrorDa@users.noreply.github.com>
2022-03-06 01:01:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,42 @@
 
 
 double pown(double value, uint16_t n) {
-
+	if (n == 0)
+		return 1;
+	else
+		return value * (pown(value, n - 1));
 }
 
 uint64_t fact(uint16_t n) {
-
+	if (n <= 1)
+		return 1;
+	else
+		return n * (fact(n - 1));
 }
 
 double calcItem(double x, uint16_t n) {
-
+	return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+	if (count == 0)
+		return 1;
+	else
+		return calcItem(x, count) + expn(x, count - 1);
 }
 
 double sinn(double x, uint16_t count) {
+	if (count <= 0)
+		return x;
+	else
+		return pown(-1, (count - 1)) * calcItem(x, (2 * count - 1)) + pown(-1, (count - 2)) * calcItem(x, (2 * count - 2));
 
 }
 
 double cosn(double x, uint16_t count) {
+	if (count <= 0)
+		return x;
+	else
+		return pown(-1, (count - 1)) * calcItem(x, (2 * count - 2)) + pown(-1, (count - 3)) * calcItem(x, (2 * count - 3));
 
 }
